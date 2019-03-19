480e3b557db6057ebd1a09254a351260396e8cfe
Andrew Usher <andrewusher00@gmail.com>
2019-03-19 09:17:41 -0500

    Move constants into seperate module

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+const allNodeList = document.querySelectorAll('clock *')
+const ALL_ELEMENTS = [...allNodeList]
+const PREFIX_ELEMENTS = document.querySelectorAll('.prefix')
+const SUFFIX_ELEMENTS = document.querySelectorAll('.suffix')
+
+export { ALL_ELEMENTS, PREFIX_ELEMENTS, SUFFIX_ELEMENTS }
