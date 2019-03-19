aac3157d0e3fcd459f916db6f4356ba5220b264f
Andrew Usher <andrewusher00@gmail.com>
2019-03-19 09:36:38 -0500

    Remove console.log in populateThemeSelector

--- src/populateThemeSelector.js
+++ src/populateThemeSelector.js
@@ -3,10 +3,7 @@ import themes from './themes'
 
 const populateThemeSelector = () => {
   $('select').innerHTML += Object.keys(themes)
-    .map(
-      theme =>
-        console.log(theme) || `<option value="${theme}">${theme}</option>`
-    )
+    .map(theme => `<option value="${theme}">${theme}</option>`)
     .join('')
 }
 
