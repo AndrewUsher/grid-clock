16fe0239eaec89aac1e37e170a6fb13d275188c3
Andrew Usher <andrewusher00@gmail.com>
2019-03-19 09:35:23 -0500

    Add jsdoc annotations to clearClock

--- src/clearClock.js
+++ src/clearClock.js
@@ -1,5 +1,11 @@
 import { ALL_ELEMENTS } from './constants'
 
+/**
+ * @description Turns all clock elements off
+ * @function clearClock
+ * @public
+ *
+ */
 function clearClock () {
   ALL_ELEMENTS.forEach(element => {
     element.classList.remove('on')
