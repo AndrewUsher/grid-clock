ece7aefcab5b87dcc82f36e3895e4945c52a8c45
Andrew Usher <andrewusher00@gmail.com>
2019-03-20 16:21:56 -0500

    Change requires to imports Make window fullscreen by default Comments cleanup

--- src/main/index.js
+++ src/main/index.js
@@ -1,15 +1,15 @@
 import { format as formatUrl } from 'url'
-const { app, BrowserWindow } = require('electron')
-const { join } = require('path')
+import { app, BrowserWindow } from 'electron'
+import { join } from 'path'
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let window
 
 function createWindow () {
-  window = new BrowserWindow()
+  window = new BrowserWindow({
+    fullscreen: true
+  })
   if (isDevelopment) {
     window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`)
   } else {
@@ -29,7 +29,6 @@ function createWindow () {
 
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
@@ -37,12 +36,7 @@ app.on('window-all-closed', () => {
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (window === null) {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
